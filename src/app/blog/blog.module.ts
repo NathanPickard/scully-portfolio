11a0c9a5d5817f05e79bd1f695064723b0ceea20
Nathan Pickard <nathan.pickard87@gmail.com>
2020-10-06 10:36:32 -0700

    Router link active options

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,11 +1,20 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {ScullyLibModule} from '@scullyio/ng-lib';
-import {BlogRoutingModule} from './blog-routing.module';
-import {BlogComponent} from './blog.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { BlogRoutingModule } from './blog-routing.module';
+import { BlogComponent } from './blog.component';
+
+import { MaterialModule } from '../material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [BlogComponent],
-  imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
+  imports: [
+    CommonModule,
+    BlogRoutingModule,
+    ScullyLibModule,
+    MaterialModule,
+    FlexLayoutModule
+  ]
 })
-export class BlogModule {}
+export class BlogModule { }
