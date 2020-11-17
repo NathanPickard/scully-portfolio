32316f083bac46f2822f195c2b196900cc69a663
Nathan Pickard <nathan.pickard87@gmail.com>
2020-11-16 18:55:50 -0800

    Update to Angular 11

--- src/app/portfolio-root/portfolio-root.component.spec.ts
+++ src/app/portfolio-root/portfolio-root.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PortfolioRootComponent } from './portfolio-root.component';
 
@@ -6,7 +6,7 @@ describe('PortfolioRootComponent', () => {
   let component: PortfolioRootComponent;
   let fixture: ComponentFixture<PortfolioRootComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PortfolioRootComponent ]
     })
