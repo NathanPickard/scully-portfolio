b518aa5b9a33c02ef7fde90975b9d3f2996b18a6
Nathan Pickard <nathan.pickard87@gmail.com>
2021-02-12 18:36:13 -0800

    Move sidenav to sidenav component

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {RouterOutlet } from '@angular/router';
+
+import { SideNavService } from '../../side-nav.service';
+
+import { routeTransitionAnimations } from '../../route-transition-animations'
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
+  animations: [routeTransitionAnimations]
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('sidenav', { static: true }) sidenav;
 
-  ngOnInit(): void {
+  constructor(private sideNavService: SideNavService) { }
+  
+  title = 'scully-portfolio';
+
+  routes = [
+    { path: '/blog', name: 'Blog' },
+    { path: '/portfolio', name: 'Portfolio' },
+    { path: '/about', name: 'About' }
+  ];
+
+  ngOnInit() {
+    this.sidenav.open();
+
+    this.sideNavService.sideNavToggleSubject.subscribe(() => {
+      this.sidenav.toggle();
+    });
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animationState'];
   }
 
 }
