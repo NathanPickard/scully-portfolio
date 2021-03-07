081c4f655cd683ffe0957aa0b10356c4b2b17100
Nathan Pickard <nathan.pickard87@gmail.com>
2021-03-06 20:02:41 -0800

    Import pageAnimations in blog root component

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,12 +1,14 @@
 import { trigger, transition, style, query, animate, animateChild, group, stagger } from '@angular/animations';
 
-export const pageAnimation =
+export const pageAnimations =
 
   trigger('pageAnimations', [
     transition(':enter', [
+      // query('.post', [
         style({opacity: 0, transform: 'translateY(-100px)'}),
         stagger(-30, [
           animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
         ])
-      ])
-    ]);
+      // ])
+    ])
+  ]);
--- src/app/blog-root/blog-root.component.ts
+++ src/app/blog-root/blog-root.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
+
+// import { pageAnimations } from '../animations';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -8,9 +10,12 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-blog-root',
   templateUrl: './blog-root.component.html',
-  styleUrls: ['./blog-root.component.scss']
+  styleUrls: ['./blog-root.component.scss'],
+  // animations: [pageAnimations]
 })
 export class BlogRootComponent implements OnInit {
+  // @HostBinding('@pageAnimation')
+
   // links$: Observable<ScullyRoute[]> = this.scully.available$;
   posts$: Observable<ScullyRoute[]>;
   // filteredRoutes: Observable<ScullyRoute[]>;
